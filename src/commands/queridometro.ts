d7997e99f51a40597ae11008ecd351d8d9f8fec5
callsamu <samuelbree@gmail.com>
2023-04-07 17:07:36 -0300

    implement queridometro command

---
+++ src/commands/queridometro.ts
@@ -0,0 +1,59 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, EmbedBuilder, MessageComponentInteraction, SlashCommandBuilder } from "discord.js";
+import { getGuildReactions } from "../storage";
+import { listReactions } from "../helpers";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('queridometro')
+        .setDescription('start queridometro voting'),
+    async execute(interaction: CommandInteraction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const row = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('primary')
+                    .setLabel('Join voting')
+                    .setStyle(ButtonStyle.Primary),
+            );
+
+        if (!interaction.guild) {
+            await interaction.followUp({
+                content: 'unable to identify guild',
+                ephemeral: true
+            });
+            return;
+        }
+        const reactions = await getGuildReactions(interaction.guild.id);
+        const reactionsInString = listReactions(reactions);
+
+        const response = await interaction.reply({
+            content: `QUINTO QUERIDOMETRO DA PQP\n${reactionsInString}`,
+            components: [row]
+        })
+
+        const filter = (i: MessageComponentInteraction) => i.customId === 'primary';
+
+        const collector = response.createMessageComponentCollector({
+            filter, time: 10000000
+        });
+
+        collector.on('collect', async i => {
+            const embed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(i.user.username)
+                .setThumbnail(i.user.avatarURL());
+
+            if (i.channel) {
+                const message = await i.reply({
+                    embeds: [embed],
+                    fetchReply: true
+                });
+
+                reactions.forEach((_, emoji) => {
+                    message.react(emoji);
+                });
+            }
+        });
+    }
+}
