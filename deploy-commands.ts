45fd698de71ad80ca5e51b464d11d7710c145ebc
callsamu <samuelbree@gmail.com>
2023-04-06 21:26:22 -0300

    fix command deployment script

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,19 +1,10 @@
-import { REST, Routes } from 'discord.js';
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
 import path from 'node:path';
 
-import { Command } from 'src/command';
-import { importAllFromDir } from 'src/helpers';
+import { Command } from './src/command';
+import { importAllFromDir } from './src/helpers';
 
-const commands = [];
-
-importAllFromDir('./src/commands', obj => {
-    command: Command = obj;
-    commands.push(command.data.toJSON());
-});
-
-for (const file of commandFiles) {
-    const command = require(`./src/commands/${file}`);
-}
+const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
 
 const token = process.env.DISCORD_TOKEN;
 if (!token) {
@@ -31,7 +22,13 @@ if (!clientId || !guildId) {
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
+    await importAllFromDir('commands', obj => {
+        const command: Command = obj;
+        commands.push(command.data.toJSON());
+    });
+
     try {
+
         console.log(`started refreshing ${commands.length} application (/) commands`);
 
         await rest.put(
